ef70e40ad711358f0d4a0b5da50709d10c33d689
Ayush <ayushshivhare51@gmail.com>
2025-05-04 13:14:51 +0530

    feat: signup successful

--- client/src/pages/Auth/Signup.tsx
+++ client/src/pages/Auth/Signup.tsx
@@ -2,12 +2,26 @@ import { z } from "zod";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { useMutation } from "@apollo/client";
+import { signUp } from "../../graphql/mutations/Auth";
+
 const userSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
 
 export default function Signup() {
+  const [registerUser, { loading }] = useMutation(signUp, {
+    onCompleted: (data) => {
+      if (data) {
+        alert("User registered successfully");
+      }
+    },
+    onError: (error) => {
+      console.error("Error signing up:", error);
+    },
+  });
+
   const {
     register,
     handleSubmit,
@@ -21,7 +35,16 @@ export default function Signup() {
   });
 
   const onSubmit: SubmitHandler<z.infer<typeof userSchema>> = async (data) => {
-    console.log(data);
+    try {
+      registerUser({
+        variables: {
+          email: data.email,
+          password: data.password,
+        },
+      });
+    } catch (error) {
+      alert(error);
+    }
   };
 
   return (
@@ -61,8 +84,9 @@ export default function Signup() {
                 <span style={{ color: "red" }}>{errors.password.message}</span>
               )}
             </div>
-            <button className="submitButton" type="submit">
-              Signup
+
+            <button disabled={loading} className="submitButton" type="submit">
+              {loading ? <span>Loading...</span> : <span>Signup</span>}
             </button>
           </form>
         </section>
